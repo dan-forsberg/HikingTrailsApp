9638c4e5f2e7f1efc308049dd78f600a19d1ba6c
Dan Örjestad <danorjest@gmail.com>
2019-02-03 17:34:30 +0100

    Added error messages when making empty requests

--- controllers/admin-api.js
+++ controllers/admin-api.js
@@ -1,8 +1,28 @@
+/* If a request gets here, it's malformed -- inform user of that */
+
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-	res.send('GET');
+
+function req_not_valid(req_type = null, msg = null) {
+	return `{ succeeded: false, msg: 'Invalid request ${req_type}. ${msg}' }`;
+}
+
+router.get('/', (req, res) => {
+	res.status(500);
+	res.send(req_not_valid, 'GET', 'Use /api to show data instead.');
+});
+router.post('/', (req, res) => {
+	res.status(500);
+	res.send(req_not_valid, 'POST', 'Bundle, path, or place must be specified. Requires data to add.');
+});
+router.put('/', (req, res) => {
+	res.status(500);
+	res.send(req_not_valid('PUT', 'Bundle, path, or place must be specified, along with ID. Requires object of updates.'));
+});
+router.delete('/', (req, res) => {
+	res.status(500);
+	res.send(req_not_valid('DELETE', 'Bundle, path, or place must be specified, along with ID.'));
 });
 
 module.exports = router;
