4414fc4266904bcc5e6684a8a5b5a934d5d711d8
Dan Örjestad <danorjest@gmail.com>
2019-02-04 17:46:16 +0100

    Added POST to be able to add Place

--- controllers/admin-place.js
+++ controllers/admin-place.js
@@ -2,6 +2,35 @@ const express = require('express');
 const router = express.Router();
 const place = require('../models/place');
 
+router.get('/', (req, res) => {
+	res.send({succeeded: false, msg: 'Use api/ for GETs'});
+});
 
+router.post('/', (req, res) => {
+	const reqPlace = req.body.newPlace;
+	let errs = [];
+
+	if (!reqPlace) {
+		errs.push('Got no newPlace');
+	}
+	if (!reqPlace === {}) {
+		errs.push('New place empty');
+	}
+	/* further validation could be added here... */
+	if (errs.length)
+		res.send({succeeded: false, msg: errs});
+
+	const newPlace = new place.Place(reqPlace);
+	place.addPlace(newPlace).then((pl) => {
+		res.send(pl)
+	})
+
+	.catch((err) => {
+		res.status(500);
+		res.send("Internal error.");
+		console.error(err);
+	});
+
+});
 
 module.exports = router;
