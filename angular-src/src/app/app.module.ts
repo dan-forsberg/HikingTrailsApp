5f1d0ee32a956461c1d6ccad3af17a9758669cd2
Dan Örjestad <danorjest@gmail.com>
2019-02-05 20:08:03 +0100

    Added service-skeletons for Bundle, Path, Place

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ViewBundleComponent } from './view-bundle/view-bundle.component';
+
+import { BundleService } from './services/bundle.service';
+import { PathService } from './services/path.service';
+import { PlaceService } from './services/place.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewBundleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [BundleService, PathService, PlaceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular-src/src/app/view-bundle/view-bundle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-bundle',
+  templateUrl: './view-bundle.component.html',
+  styleUrls: ['./view-bundle.component.css']
+})
+export class ViewBundleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
