175c66f70497b6ae27e7acda327299437fa47fc1
Dan Örjestad <danorjest@gmail.com>
2019-02-06 20:14:59 +0100

    Added forms module

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ViewBundleComponent } from './view-bundle/view-bundle.component';
@@ -19,6 +20,7 @@ import { AddBundleComponent } from './add-bundle/add-bundle.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [BundleService, PathService, PlaceService],
   bootstrap: [AppComponent]
